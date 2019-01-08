d6d7fbc19f712e7f53a155b593b0db0901198f46
Vasia228 <43805731+Vasia228@users.noreply.github.com>
2019-01-09 00:09:40 +0300

    Add files via upload

---
+++ src/Automato.cpp
@@ -0,0 +1,120 @@
+#include "Automato.h"
+
+const string drinks[3] {"Nescafe","Neste","Nuri"};
+const int price[3] {25,31,18};
+const int size_of_menu=3;
+
+Automato::Automato()
+{
+    state="off";
+    cash=0;
+}
+
+Automato::~Automato()
+{
+    //dtor
+}
+
+void Automato::on()
+{
+    if (state=="off")
+    {
+        state="wait";
+    }
+}
+
+void Automato::off()
+{
+    state="off";
+}
+
+void Automato::menu()
+{
+    for (int i=0;i<size_of_menu;i++)
+    {
+        cout<<i+1<<")"<<drinks[i]<<" - "<<price[i]<<"p"<<endl;
+    }
+}
+
+void Automato::coin(int money)
+{
+    if (state=="wait")
+    {
+        state="accept";
+    }
+    if (state=="accept")
+    {
+        cash+=money;
+    }
+}
+
+void Automato::cancel()
+{
+    if (state=="accept")
+    {
+        cash=0;
+        state="wait";
+    }
+}
+
+void Automato::choice(int position)
+{
+    if (state=="accept")
+    {
+        position--;
+        if (position > 0 && position < size_of_menu)
+        {
+            state="check";
+            if (cash >= price[position])
+            {
+                cash-=price[position];
+                cash=0; //change
+                this->cook(position);
+            }
+            else
+            {
+				state="accept";
+            }
+        }
+    }
+}
+
+void Automato::cook(int position)
+{
+    state="cook";
+    //cooked
+    this->finish();
+}
+
+void Automato::finish()
+{
+    state="wait";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Automato.h"
+
+using namespace std;
+
+int main()
+{
+    Automato Coffe;
+    Coffe.on();
+    Coffe.coin(10);
+    Coffe.choice(3);
+    Coffe.menu();
+    Coffe.coin(5);
+    Coffe.coin(3);
+    Coffe.choice(3);
+}
