54ebb3b1697452e6b20ebb3766aa6cf81650919d
zile <elizabet3000@mail.ru>
2019-01-08 19:19:26 +0300

    2 tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,13 +5,14 @@ TEST(lab4, test1)
 {
     Automata * automata = new Automata();
     automata -> choice(1);
-    EXPECT_EQ(automata -> getSatate(), OFF);
+    int i = (automata -> getSatate() == OFF);
+    EXPECT_EQ(1, i);
 }
 
 TEST(lab4, test2)
 {
     Automata * automata = new Automata();
     automata -> on();
-    int i = automata -> getSatate() == WAIT;
+    int i = (automata -> getSatate() == WAIT);
     EXPECT_EQ(1, i);
 }
