4cc70ab76133168f3b632f0f2a9e746962357725
Vasia228 <43805731+Vasia228@users.noreply.github.com>
2019-01-09 00:05:57 +0300

    Add files via upload

---
+++ src/Automato.h
@@ -0,0 +1,31 @@
+#ifndef AUTOMATO_H
+#define AUTOMATO_H
+using namespace std;
+#include<string>
+#include<iostream>
+
+class Automato
+{
+    public:
+        string getState(){return state;};
+        int getCash(){return cash;};
+        Automato();
+        virtual ~Automato();
+        void on();
+        void off();
+        void menu();
+        void coin(int);
+        void cancel();
+        void choice(int);
+
+    protected:
+
+    private:
+        string state;
+        int cash;
+
+        void cook(int);
+        void finish();
+};
+
+#endif // AUTOMATO_H
