d36f9df296282b09bc33e13dffce27720b0a630c
Artem Aroslankin <43597601+orrrrtem@users.noreply.github.com>
2019-01-09 11:15:09 +0300

    Update automata.h

--- include/automata.h
+++ include/automata.h
@@ -11,9 +11,6 @@
 #include <thread>
 
 using namespace std;
-using namespace std::this_thread;     // sleep_for, sleep_until
-using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
-using std::chrono::system_clock;
 
 enum STATES {OFF, WAIT, ACCEPT, CHECK, COOK};
 class Automata{
