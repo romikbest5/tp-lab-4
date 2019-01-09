46fb3115ccefa317a120e7b83687fc14c8d7a064
iamilyasedunov <36935089+iamilyasedunov@users.noreply.github.com>
2019-01-09 11:46:53 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -2,6 +2,19 @@
 // Created by Илья on 07.01.2019.
 //
 #include "../include/headers.h"
+#include <iostream>
+#include <string>
+
+enum STATES
+{
+    OFF,
+    WAIT,
+    ACCEPT,
+    CHECK,
+    COOK,
+    error
+};
+
 
 const int N = 4;
 
