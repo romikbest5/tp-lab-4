a9f776ea5893cd0212b5c09a0b874ddd375edb3c
Artem Aroslankin <43597601+orrrrtem@users.noreply.github.com>
2019-01-13 22:22:14 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,16 +1,25 @@
 #include "gtest/gtest.h"
 #include "automata.h"
+#include <string>
+
+using namespace std;
 
 
 TEST(lab4,scene1)
 {
     Automata nc;
-    nc.on();
+    string h = nc.on();
+    EXPECT_EQ("THE MACHINE HAS TURNED ON",h);
 
-    
     nc.coin(10);
     int c = nc.getcash();
     EXPECT_EQ(10,c);
+
+    nc.coin(30);
+    EXPECT_EQ(ACCEPT, nc.getstate())
+
+    string ans = nc.choice(2);
+    EXPECT_EQ("ENJOY YOUR DRINK",ans);
 }
 
 
