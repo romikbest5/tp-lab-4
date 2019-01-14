a0ec5d26b596fe8cd09cad641bbee617d3bd52d1
Artem Aroslankin <43597601+orrrrtem@users.noreply.github.com>
2019-01-14 17:32:02 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-
+//simple
 TEST(lab4,scene1)
 {
     Automata nc;
@@ -20,6 +20,67 @@ TEST(lab4,scene1)
 
     string ans = nc.choice(2);
     EXPECT_EQ("ENJOY YOUR DRINK",ans);
+}
+// double on()
+TEST(lab4,scene2)
+{
+    Automata nc;
+    string h = nc.on();
+    EXPECT_EQ("THE MACHINE HAS TURNED ON",h);
+    h = nc.on();
+    EXPECT_EQ("THE MACHINE HAS ALREADY TURNED ON",h);
+
+    nc.coin(10);
+    int c = nc.getcash();
+    EXPECT_EQ(10,c);
+
+    nc.coin(30);
+    EXPECT_EQ(ACCEPT, nc.getstate());
+
+    string ans = nc.choice(2);
+    EXPECT_EQ("ENJOY YOUR DRINK",ans);
+}
+//off
+TEST(lab4,scene3)
+{
+    Automata nc;
+    string h = nc.on();
+    EXPECT_EQ("THE MACHINE HAS TURNED ON",h);
+
+    nc.coin(10);
+    int c = nc.getcash();
+    EXPECT_EQ(10,c);
+
+    nc.coin(30);
+    EXPECT_EQ(ACCEPT, nc.getstate());
+
+    string ans = nc.choice(2);
+    EXPECT_EQ("ENJOY YOUR DRINK",ans);
+    
+    ans = nc.off();
+    EXPECT_EQ("THE MACHINE HAS TURNED OFF",ans);
+}
+
+//not enough money
+TEST(lab4,scene4)
+{
+    Automata nc;
+    nc.on();
+    nc.coin(10);
+    string ans = nc.choice(2);
+    EXPECT_EQ("ERROR. NOT ENOUGHT MONEY",ans);
+    nc.off();
+}
+
+//wrong number of drink
+TEST(lab4,scene5)
+{
+    Automata nc;
+    nc.on();
+    nc.coin(50);
+    string ans = nc.choice(16);
+    EXPECT_EQ("ERROR. INVALID NUMBER",ans);
+    nc.off();
 }
 
 
