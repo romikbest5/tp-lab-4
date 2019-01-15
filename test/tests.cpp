c95c44fcc011e795588b5127dd9a4c6fdf82089c
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-01-16 02:49:33 +0300

    11

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,35 +1,32 @@
 #include "gtest/gtest.h"
 #include "Automata.h"
 
-TEST(lab4, task_1) {
+TEST(lab4, task1_1)
+{
 	Automata automata1;
 	automata1.on();
-	STATES state = automata1.getState();
-	STATES cond = IDLE;
+	AState state; = automata1.getState();
+	AState cond = aWait;
 	EXPECT_EQ(cond, state);
 }
 TEST(lab4, task_2) {
 	Automata automata1;
 	automata1.on();
-	automata1.printMenu();
+	automata1.printState();
 	automata1.coin(100);
 	automata1.choice(3);
-	unsigned int cash = automata1.getCash();
-	EXPECT_EQ(90, cash);
+	EXPECT_EQ(90, coins);
 }
 TEST(lab4, task_3) {
 	Automata automata;
 	automata.on();
 
-	unsigned int cash = automata.getCash();
-	EXPECT_EQ(0, cash);
+	EXPECT_EQ(0, coins);
 }
 TEST(lab4, task_4) {
 	Automata automata;
 	automata.on();
 	automata.coin(1);
 	automata.choice(2);
-
-	unsigned int cash = automata.getCash();
-	EXPECT_EQ(1, cash);
+	EXPECT_EQ(1, coins);
 }
