aca5c6717957cfa3c15dbeba368edc84a8ccb7dc
Artem Aroslankin <43597601+orrrrtem@users.noreply.github.com>
2019-01-15 15:33:03 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -82,6 +82,79 @@ TEST(lab4,scene5)
     EXPECT_EQ("ERROR. INVALID NUMBER",ans);
     nc.off();
 }
+//another one simple (WAIT AFTER FINISH)
+TEST(lab4,scene6)
+{
+    Automata nc;
+    string h = nc.on();
+    EXPECT_EQ("THE MACHINE HAS TURNED ON",h);
 
+    nc.coin(10);
+    int c = nc.getcash();
+    EXPECT_EQ(10,c);
+
+    nc.coin(30);
+    EXPECT_EQ(ACCEPT, nc.getstate());
+
+    string ans = nc.choice(2);
+    EXPECT_EQ("ENJOY YOUR DRINK",ans);
+    
+    EXPECT_EQ(WAIT, nc.getstate());
+}
+
+//another one simple (CASH AFTER FINISH)
+TEST(lab4,scene7)
+{
+    Automata nc;
+    string h = nc.on();
+    EXPECT_EQ("THE MACHINE HAS TURNED ON",h);
+
+    nc.coin(10);
+    int c = nc.getcash();
+    EXPECT_EQ(10,c);
+
+    nc.coin(30);
+    EXPECT_EQ(ACCEPT, nc.getstate());
+
+    string ans = nc.choice(2);
+    EXPECT_EQ("ENJOY YOUR DRINK",ans);
+    
+    EXPECT_EQ(WAIT, nc.getstate());
+    
+    EXPECT_EQ(0, nc.getcash());
+}
+
+
+// choosing after an error(number of drink)
+TEST(lab4,scene8)
+{
+     Automata nc;
+    nc.on();
+    nc.coin(50);
+    nc.choice(16);
+    
+    string ans = nc.choice(1);
+    EXPECT_EQ("ENJOY YOUR DRINK",ans);
+    
+    nc.off();
+}
+
+// choosing after an error(money)
+TEST(lab4,scene9)
+{
+    Automata nc;
+    nc.on();
+    nc.coin(10);
+    string ans = nc.choice(1);
+    EXPECT_EQ("ERROR. NOT ENOUGHT MONEY",ans);
+    
+    nc.coin(10);
+    EXPECT_EQ(20,nc.getcash());
+    
+    ans = nc.choice(1);
+    EXPECT_EQ("ENJOY YOUR DRINK",ans);
+    
+    nc.off();
+}
 
 
