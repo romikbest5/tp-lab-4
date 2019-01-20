7ed05f05714893a822c0dd1a2ba1f8117224719f
Chernozemova <44014905+Chernozemova@users.noreply.github.com>
2019-01-20 16:58:24 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,21 +2,22 @@
 #include "Automata.h"
 #include <string>
 
-Automata a;
-
 TEST(Lab4, test1) {
+	Automata a;
 	string str = a.on();
   EXPECT_EQ(str, "ON");
 }
 
 
 TEST(Lab4, test2) {
+	Automata a;
 	a.on();
 	string str = a.off();
 	EXPECT_EQ(str, "OFF");
 }
 
 TEST(Lab4, test3) {
+	Automata a;
 	a.on();
 	a.coin(20);
 	a.coin(4);
@@ -26,6 +27,7 @@ TEST(Lab4, test3) {
 }
 
 TEST(Lab4, test4) {
+	Automata a;
 	a.on();
 	a.coin(20);
 	a.coin(4);
@@ -36,6 +38,7 @@ TEST(Lab4, test4) {
 }
 
 TEST(Lab4, test5_change) {
+	Automata a;
 	a.coin(50);
 	a.choice(3);
 	int k = a.getCash();
@@ -43,6 +46,7 @@ TEST(Lab4, test5_change) {
 }
 
 TEST(Lab4, test6) {
+	Automata a;
 	a.coin(23);
 	a.choice(3);
 	int k = a.getCash();
@@ -50,9 +54,10 @@ TEST(Lab4, test6) {
 }
 
 TEST(Lab4, test7) {
+	Automata a;
 	a.coin(23);
 	a.choice(3);
 	a.coin(10);
-	int k = a.getCash(); 
+	int k = a.getCash();
 	EXPECT_EQ(k, 3);
 }
