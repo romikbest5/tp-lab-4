caed81e510e4be527ee2df2938dd050b71500561
unknown <aisidneva@edu.hse.com>
2019-01-31 15:34:44 +0300

    update tests 2

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,71 +3,88 @@
 
 TEST(lab4,task1)
 {
+	STATES res;
 	Automata drink;
-	EXPECT_EQ(STATES::OFF,drink.getState());
+	res = drink.getState();
+	EXPECT_EQ(STATES::OFF, res);
 }
 
 TEST(lab4,task2)
 {
+	STATES res;
 	Automata drink;
 	drink.on();
-	EXPECT_EQ(STATES::WAIT,drink.getState());
+	res = drink.getState();
+	EXPECT_EQ(STATES::WAIT, res);
 }
 
 TEST(lab4,task3)
 {
+	STATES res;
 	Automata drink;
 	drink.on();
 	drink.coin(-1);
-	EXPECT_EQ(STATES::WAIT,drink.getState());
+	res = drink.getState();
+	EXPECT_EQ(STATES::WAIT, res);
 }
 
 TEST(lab4,task4)
 {
+	STATES res;
 	Automata drink;
 	drink.on();
 	drink.coin(1);
-	EXPECT_EQ(STATES::ACCEPT,drink.getState());
+	res = drink.getState();
+	EXPECT_EQ(STATES::ACCEPT, res);
 }
 
 TEST(lab4,task5)
 {
+	STATES res;
 	Automata drink;
 	drink.on();
 	drink.coin(1);
 	drink.choice(-1);
-	EXPECT_EQ(STATES::ACCEPT,drink.getState());
+	res = drink.getState();
+	EXPECT_EQ(STATES::ACCEPT, res);
 }
 
 TEST(lab4,task6)
 {
+	STATES res;
 	Automata drink;
 	drink.on();
 	drink.coin(1);
 	drink.choice(0);
-	EXPECT_EQ(STATES::CHECK,drink.getState());
+	res = drink.getState();
+	EXPECT_EQ(STATES::CHECK, res);
 }
 
 TEST(lab4,task7)
 {
+	bool res;
     Automata drink;
 	drink.on();
 	drink.coin(40);
 	drink.choice(1);
-    EXPECT_EQ(true, drink.check(1));
+	res = drink.check(1);
+    EXPECT_EQ(true, res);
 }
 
 TEST(lab4,task8)
 {
+	bool res;
 	Automata drink;
 	drink.on();
 	drink.coin(29);
 	drink.choice(3);
-    EXPECT_EQ(false, drink.check(3));
+	res = drink.check(3);
+    EXPECT_EQ(false, res);
 }
 
 TEST(lab4,task9)
 {
+	bool res;
 	Automata drink;
 	drink.on();
 	drink.coin(44);
@@ -79,13 +96,16 @@ TEST(lab4,task9)
 		while (!drink.check(2)) {
 			drink.coin(3);
 		}
+		drink.cook();
 	}
-    EXPECT_EQ(true, drink.check(2));
+	res = drink.check(2);
+    EXPECT_EQ(true, res);
 }
 
 TEST(lab4,task10)
 {
-		Automata drink;
+	STATES res;
+	Automata drink;
 	drink.on();
 	drink.coin(44);
 	drink.choice(2);
@@ -96,13 +116,16 @@ TEST(lab4,task10)
 		while (!drink.check(2)) {
 			drink.coin(3);
 		}
+		drink.cook();
 	}
-    EXPECT_EQ(STATES::COOK, drink.getState());
+	res = drink.getState();
+    EXPECT_EQ(STATES::COOK, res);
 }
 
 TEST(lab4,task11)
 {
-		Automata drink;
+	STATES res;
+	Automata drink;
 	drink.on();
 	drink.coin(44);
 	drink.choice(2);
@@ -113,14 +136,17 @@ TEST(lab4,task11)
 		while (!drink.check(2)) {
 			drink.coin(3);
 		}
+		drink.cook();
 	}
 	drink.finish();
-    EXPECT_EQ(STATES::OFF, drink.getState());
+	res = drink.getState();
+    EXPECT_EQ(STATES::OFF, res);
 }
 
 TEST(lab4,task12)
 {
-		Automata drink;
+	STATES res;
+	Automata drink;
 	drink.on();
 	drink.coin(44);
 	drink.choice(2);
@@ -131,14 +157,17 @@ TEST(lab4,task12)
 		while (!drink.check(2)) {
 			drink.coin(10);
 		}
+		drink.cook();
 	}
 	drink.finish();
-    EXPECT_EQ(STATES::WAIT, drink.getState());
+	res = drink.getState();
+    EXPECT_EQ(STATES::WAIT, res);
 }
 
 TEST(lab4,task13)
 {
-		Automata drink;
+	STATES res;
+	Automata drink;
 	drink.on();
 	drink.coin(44);
 	drink.choice(2);
@@ -148,5 +177,6 @@ TEST(lab4,task13)
 		drink.contined(0);
 	}
 	drink.finish();
-    EXPECT_EQ(STATES::WAIT, drink.getState());
+	res = drink.getState();
+    EXPECT_EQ(STATES::WAIT, res);
 }
