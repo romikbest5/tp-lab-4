f9b45f3ac6f087b7a1c6f7f89d7d517a28f655d9
AlexSlav <alexander.slavutin@rambler.ru>
2019-02-26 18:12:27 +0300

    New files

--- include/Automata.h
+++ include/Automata.h
@@ -7,8 +7,23 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
 class Automata {
 private:
+    int cash;
+    enum STATES {OFF, WAIT, ACCEPT, CHECK, COOK};
+    string menu[5]={"milk","tea","coffee","espresso","juice"};
+    int prices [5]={10, 12, 15, 20, 25};
+    STATES state;
+
+public:
+    void on(){};
+    void off(){};
+    void coin(){};
+    void on(){};
+    void on(){};
+
+
 
 };
 
