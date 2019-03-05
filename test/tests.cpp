b267360cb2a54ce8e7d0d072b028374099f82dfa
MariaIsupova <Mariaisisupova@gmail.com>
2019-03-05 13:59:13 +0300

    test2

--- test/tests.cpp
+++ test/tests.cpp
@@ -12,15 +12,15 @@ TEST(lab4,coin)
 	
 }
 
-TEST(lab4,choice)
+TEST(lab4,choice_1)
 {
     Automata machine;
 	machine.on();
 	EXPECT_EQ("This position is not found", machine.choice(9));
 	machine.coin(10);
-	EXPECT_EQ("The balance is 0... Deposit the money.", machine.choise(6));
+	EXPECT_EQ("The balance is 0... Deposit the money.", machine.choice(6));
 	machine.coin(10);
-	EXPECT_EQ(6, machine.choise(6));
+	EXPECT_EQ(6, machine.choice(6));
 }
 
 TEST(lab4,check_1)
@@ -41,7 +41,7 @@ TEST(lab4,check_2)
 	bool result = machine.check(5);
 	EXPECT_EQ(expected, result);
 }
-TEST(lab4,choice)
+TEST(lab4,choice_2)
 {
     Automata machine;
 	machine.on();
