1d8a8959ade4f4b1d12d6b4cd7f7216e14b41d58
Pavel Semkin <pvsemk@mail.ru>
2019-03-12 23:21:02 +0300

    Automata.cpp brackets for functions are inserted

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,3 +1,56 @@
 #include "Automata.h"
 
+Automata::Automata()
+{
+
+}
+
+STATES Automata::Automata::on()
+{
+
+}
+
+STATES Automata::off()
+{
+
+}
+STATES Automata::coin(int sum)
+{
+
+}
+
+void Automata::printMenu()
+{
+
+}
+
+void Automata::printState()
+{
+
+}
+
+STATES Automata::choice(int option)
+{
+
+}
+
+bool Automata::check()
+{
+
+}
+
+STATES Automata::cancel()
+{
+
+}
+
+STATES Automata:: cook()
+{
+
+}
+
+std::string Automata::finish()
+{
+
+}
 
