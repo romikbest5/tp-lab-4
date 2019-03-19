07e474c7199b88e5d5e355e5f501aee63c9b038a
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-03-19 20:39:49 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,47 @@
 #include "gtest/gtest.h"
-#include "Automata.h"
+#include "Header.h"
+/*
+TEST(lab4, task_1) 
+{
+	Automata automata;
+	automata.on();
+	unsigned int cash = automata.getCash();
+	EXPECT_EQ(0, cash);
+}
 
+TEST(lab4, task_2) 
+{
+	Automata automata;
+	automata.on();
+	automata.coin(50);
+	automata.choice(3);
+	automata.choice(3);
 
+	unsigned int cash = automata.getCash();
+	EXPECT_EQ(0, cash);
+}
+
+TEST(lab4, task_3) 
+{
+	Automata automata;
+	automata.on();
+	automata.coin(25);
+	automata.choice(1);
+
+	unsigned int cash = automata.getCash();
+	EXPECT_EQ(25, cash);
+}
+
+TEST(lab4, task_4) 
+{
+	Automata automata;
+	automata.on();
+	automata.coin(100);
+	automata.choice(3);
+	automata.choice(2);
+	automata.choice(2);
+
+	unsigned int cash = automata.getCash();
+	EXPECT_EQ(35, cash);
+}
+*/
