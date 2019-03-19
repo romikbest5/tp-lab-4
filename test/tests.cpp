7493c93181333e253884a475eb72f7d9e0486a29
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-03-19 21:51:46 +0300

    test2

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,11 +1,11 @@
 #include "gtest/gtest.h"
 #include "../include/Automata.h"
- /* TEST(lab4, task_1) 
+  TEST(lab4, task_1) 
 {
 	Automata a;
 	a.on();
-	unsigned int coins = a.getCash();
-	EXPECT_EQ(0, coins);
+	 int c = a.getCash();
+	EXPECT_EQ(0, c);
 }
 
 TEST(lab4, task_2) 
@@ -13,35 +13,30 @@ TEST(lab4, task_2)
 	Automata a;
 	a.on();
 	a.coin(50);
-	a.choice(3);
-	a.choice(3);
-
-	unsigned int coins = a.getCash();
-	EXPECT_EQ(0, coins);
+	 int c = a.getCash();
+	EXPECT_EQ(50, c);
 }
 
 TEST(lab4, task_3) 
 {
 	Automata a;
 	a.on();
-	a.coin(10);
-	automata.choice(1);
+	a.coin(20);
+	automata.choice(2);
 
-	unsigned int coins = a.getCash();
-	EXPECT_EQ(10, coins);
+	 int c = a.getCash();
+	EXPECT_EQ(0, c);
 }
 
 TEST(lab4, task_4) 
 {
 	Automata a;
 	a.on();
-	a.coin(100);
-	a.choice(3);
+	a.coin(75);
 	a.choice(2);
 	a.choice(2);
 
-	unsigned int coins = a.getCash();
-	EXPECT_EQ(35, coins);
+	 int c = a.getCash();
+	EXPECT_EQ(35, c);
 }
 
-*/
