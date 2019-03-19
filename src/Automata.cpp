118343c4e083a3732d6952b46e67c9ad13ecbe7d
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-03-19 20:22:12 +0300

    ффф122

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,133 +1,92 @@
 #include "Automata.h"
 
-TPrice prices[] =
-{
-	{ "Americano", 5 },
-	{ "Latte", 7 },
-	{ "Espresso", 10 },
-	{ NULL, 0 } // end
-};
 
 Automata::Automata()
-	{
+{
+	state = aOff;
+	this->coins = 0;
+}
+
+void Automata::on()
+{
+	if (state == aOff) {
+		this->state = aWait;
+		std::cout << "Hello!" << std::endl;
+	}
+}
+
+void Automata::off()
+{
+	if (this->state == aWait) {
 		state = aOff;
-		this->coins = 0;
+		std::cout << "Bye!" << std::endl;
 	}
+}
 
-	// Простые состояния - условия хватает
-	void Automata::on()
+void Automata::coin(unsigned int currency)
+{
+	if (this->state == aWait || this->state == aAccept)
 	{
-		if (state == aOff)
-			state = aWait;
-		else
-			std::cout << "Cant ON from" << StateNames[state] << "state" << std::endl;
+		this->coins += currency;
+		std::cout << "Cash:  " << currency << std::endl;
 	}
+}
 
-	void Automata::off()
-	{
-		if (state == aWait)
-			state = aOff;
-		else
-		std::cout << "Cant OFF from" << StateNames[state] << "state" << std::endl;
+void Automata::cancel()
+{
+	if (this->state != aOff) {
+		this->state = aWait;
+		std::cout << " Return " << coins << " coins" << std::endl;
+		coins = 0;
 	}
+}
 
-	void Automata::coin(unsigned int currency)
+void Automata::choice(unsigned int num)
+{
+	if (this->state == aWait || this->state == aAccept)
 	{
-		if (state == aWait)
+		this->state = aCheck;
+		if (check(num) == true)
 		{
-			std::cout << "Enter coin count: " << std::endl;
-			//std::cin >> coins;
-			this->coins+=currency;
-			state = aAccept;
+			this->state = aCook;
+			cook(num);
 		}
 		else
-			std::cout << "Cant ACCEPT COIN from" << StateNames[state] << "state" << std::endl;
-	}
-
-	// Отмена может произойти из двух состояний
-	void Automata::cancel()
-	{
-		switch (state)
 		{
-		case aWait:
-		case aCheck:
-			state = aAccept;
-			std::cout << "  Return" << coins << "coins" << std::endl;
-			coins = 0;
-			break;
-		default:
-			std::cout << "Cant CANCEL from" << StateNames[state] << "state" << std::endl;
-		};
-	}
-
-	void Automata::choice(unsigned int num)
-	{
-		if (state == aAccept)
-		{
-			std::cout << "Select:" << std::endl;
-			for (int i = 0; prices[i].price != 0; i++)
-				std::cout << i+1<<"-"<< prices[i].name <<":"<< prices[i].price << "coins" << std::endl;
-			std::cout << "Enter:" << std::endl;
-			//std::cin >> sel;
-			sel=num;
-			sel--;
-
-			state = aCheck;
+			cancel();
 		}
-		else
-			std::cout << "Cant CHOICE from" << StateNames[state] << "state" << std::endl;
+
 	}
+}
 
-	bool Automata::check(unsigned int sel)
+bool Automata::check(unsigned int num)
+{
+	if (this->coins >= this->price[num - 1])
 	{
-		if (state == aCheck)
-		{
-			if (prices[sel].price > this->coins)
-				std::cout << "No enought coins (" << coins << "and price is" << coins << ")" << prices[sel].price << std::endl;
-			else
-			{
-				std::cout << "Check: All ok" <<  std::endl;
-				return true;
-			}
-			// Или к примеру можно вывести "не хватает ингредиентов"
-		}
-		else
-			std::cout << "Cant CHECK in" << StateNames[state] << "state" << std::endl;
-
+		this->coins -= price[num - 1];
+		return true;
+	}
+	else
+	{
+		cout << "Not enough cash!" << endl;
 		return false;
 	}
+}
 
-	void Automata::cook(unsigned int sel)
+void Automata::cook(unsigned int num)
+{
+	if ((state == aCheck) & (check(num) == true))
 	{
-		if ((state == aCheck) & (check(sel)==true))
-		{
-			state = aCook;
-			std::cout << "Cook some" <<  std::endl;
-		}
-		else
-			std::cout << "Cant COOK from " << StateNames[state] << "state" << std::endl;
+		std::cout << "Please,wait. Your drink is cooking." << std::endl;
+		this->state = aWait;
+		std::cout << "Ready!Please, take a drink." << std::endl;
 	}
+}
 
-	void Automata::finish()
-	{
-		if (state == aCook)
-		{
-			this->coins -= prices[sel].price;
+int Automata::getCash()
+{
+	cout << "Get cash" << endl;
+	return this->coins;
+}
 
-			if (this->coins > 0)
-			{
-				std::cout << "Return" <<  coins << std::endl;
-				return this->coins;
-			}
-			this->coins = 0;
-			state = aWait;
-		}
-		else
-			std::cout << "Cant FINISH from" << StateNames[state] << "state" << std::endl;
-	}
-
-	void Automata::printState()
-	{
-		std::cout << "State:" << StateNames[state] << std::endl;
-	}
 
