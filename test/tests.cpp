7158ddedbbd197e8138e5d64e690e708f518a9f8
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-03-19 20:56:20 +0300

    testupdate

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,46 +1,46 @@
 #include "gtest/gtest.h"
 #include "Header.h"
-/* TEST(lab4, task_1) 
+ TEST(lab4, task_1) 
 {
-	Automata automata;
-	automata.on();
-	unsigned int cash = automata.getCash();
-	EXPECT_EQ(0, cash);
+	Automata a;
+	a.on();
+	unsigned int coins = a.getCash();
+	EXPECT_EQ(0, coins);
 }
 
 TEST(lab4, task_2) 
 {
-	Automata automata;
-	automata.on();
-	automata.coin(50);
-	automata.choice(3);
-	automata.choice(3);
+	Automata a;
+	a.on();
+	a.coin(50);
+	a.choice(3);
+	a.choice(3);
 
-	unsigned int cash = automata.getCash();
-	EXPECT_EQ(0, cash);
+	unsigned int coins = a.getCash();
+	EXPECT_EQ(0, coins);
 }
 
 TEST(lab4, task_3) 
 {
-	Automata automata;
-	automata.on();
-	automata.coin(25);
+	Automata a;
+	a.on();
+	a.coin(10);
 	automata.choice(1);
 
-	unsigned int cash = automata.getCash();
-	EXPECT_EQ(25, cash);
+	unsigned int coins = a.getCash();
+	EXPECT_EQ(10, coins);
 }
 
 TEST(lab4, task_4) 
 {
-	Automata automata;
-	automata.on();
-	automata.coin(100);
-	automata.choice(3);
-	automata.choice(2);
-	automata.choice(2);
+	Automata a;
+	a.on();
+	a.coin(100);
+	a.choice(3);
+	a.choice(2);
+	a.choice(2);
 
-	unsigned int cash = automata.getCash();
-	EXPECT_EQ(35, cash);
+	unsigned int coins = a.getCash();
+	EXPECT_EQ(35, coins);
 }
-*/
+
