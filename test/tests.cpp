4802d10680209a0a38d0f4e553082c6fa6fb7fb4
ArinaGorbacheva <43599396+ArinaGorbacheva@users.noreply.github.com>
2019-03-19 20:37:24 +0300

    change1

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,4 @@
 #include "gtest/gtest.h"
 #include "Automata.h"
-/*
-TEST(lab4, task1_1)
-{
-	Automata a;
-	a.on();
-	a.coin(20);
-	a.choice(2);
-	unsigned int coins=a.finish();
-	EXPECT_EQ(13, coins);
-}
 
 
